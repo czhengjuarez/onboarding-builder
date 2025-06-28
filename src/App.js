b2b2d43bbbc34e5cff9df306691f57ff7016f509
Changying Zheng <changyingzheng@Changyings-iMac-24.local>
2025-06-28 14:38:28 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Hello World</h1>
+      <p>This is your first React page!</p>
     </div>
   );
 }
